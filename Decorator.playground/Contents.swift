7125ef97f4423121e2508462c14c15c050678390
Pavel Chernyshev <titan100695@yandex.ru>
2020-11-17 23:42:16 +0300

    decorator pattern

--- Decorator.playground/Contents.swift
+++ Decorator.playground/Contents.swift
@@ -1,1 +1,61 @@
+protocol Coffee {
+    var price: Int {get set}
+    func calculatePrice() -> Int
+}
+
+protocol CoffeeDecorator: Coffee {
+    var coffee: Coffee {get set}
+    init(coffee: Coffee)
+    func calculatePrice() -> Int
+}
+
+extension CoffeeDecorator {
+    func calculatePrice() -> Int {
+        return coffee.calculatePrice() + price
+    }
+}
+
+
+class SimpleCoffee: Coffee {
+    var price: Int
+    
+    init(price: Int) {
+        self.price = price
+    }
+    
+    func calculatePrice() -> Int {
+        return price
+    }
+}
+
+class MilkDecorator: CoffeeDecorator {
+    var coffee: Coffee
+    var price: Int = 15
+    required init(coffee: Coffee) {
+        self.coffee = coffee
+    }
+}
+
+class WhipDecorator: CoffeeDecorator {
+    var coffee: Coffee
+    var price: Int = 10
+    
+    required init(coffee: Coffee) {
+        self.coffee = coffee
+    }
+}
+
+class SugarDecorator: CoffeeDecorator {
+    var coffee: Coffee
+    var price: Int = 20
+    
+    required init(coffee: Coffee) {
+        self.coffee = coffee
+    }
+}
+
+var standartCoffee = SimpleCoffee(price: 80)
+var total = WhipDecorator(coffee: MilkDecorator(coffee: standartCoffee)).calculatePrice()
+var royalCoffee = SimpleCoffee(price: 100)
+total = SugarDecorator(coffee: WhipDecorator(coffee: MilkDecorator(coffee: MilkDecorator(coffee: royalCoffee)))).calculatePrice()
 
